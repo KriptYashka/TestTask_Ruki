609c37eeea75e5c28d5dc61a67e38cffabf848bb
Егор Кучук <kriptyashka@gmail.com>
2024-02-17 15:02:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class Application:
+	def __init__(self):
+		self.controller = None
+		self.handler = None
+
+	def start(self):
+		pass
+
+
+def main():
+	app = Application()
+	app.start()
+
+
+if __name__ == '__main__':
+	main()
